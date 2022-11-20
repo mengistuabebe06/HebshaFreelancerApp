057578eaeaed783e980068497bfb708650354519
mengistuabebe06 <mengistuabebe06@gmail.com>
2022-11-20 14:41:20 +0300

    react-clinet pages

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-dark text-white mt-0 p-4 text-center'>
+        CopyRight &copy; {new Date().getFullYear()} HabeshaFreelancer
+
+    </footer>
+  )
+}
